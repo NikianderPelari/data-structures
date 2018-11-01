b5318b5ab8aa1bc81b00816db9c5d9f505a361c1
NikianderPelari <np2588@columbia.edu>
2018-11-01 10:05:24 -0400

    Create README.java

---
+++ assignment-5/README.java
@@ -0,0 +1,16 @@
+The file cityxy.txt contains a list of cities and their (X,Y) coordinates on the map. These are the vertices in the graph. 
+The file citypairs.txt lists direct, connections between pairs of cities. These links are bidirectional, if you can from NewYork to Boston,
+you can get from Boston to NewYork. These are the edges of the graph.
+
+
+If you use an IDE, import the java files and make sure that citxy.txt and citypairs.txt are placed in the project directory. 
+If you use java and javac from the command line put the files in the same directory as the source files.
+Compile all java sources and run the program Display. 
+
+
+Vertex.java & Edge.java - 
+Provided by instructor and represent the edges and vertices of a graph.
+
+
+Dijstrka.java - 
+Source code provided by instructor. Modifications were made to calculate Euclidean distances and compute shortest path between two points.
